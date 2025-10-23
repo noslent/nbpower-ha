078c5e00bfa5df7633a10185cd69ae9df7d24a11
noslent <234648082+noslent@users.noreply.github.com>
2025-10-23 13:11:30 -0300

    avoid rate limit on nbpower

--- custom_components/nbpower/const.py
+++ custom_components/nbpower/const.py
@@ -1,4 +1,4 @@
 DOMAIN = "nbpower"
-DEFAULT_SCAN_INTERVAL = 3600  # seconds
+DEFAULT_SCAN_INTERVAL = 86400  # seconds
 BASE = "https://nbpower.com"
 WIDGET_API = "https://nbp-svc.smartcmobile.com/WidgetAPI"
